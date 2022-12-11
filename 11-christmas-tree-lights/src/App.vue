0c6121e8792fb716e57c6c101548fb5e1dd74bd3
Simeon Petrov <petrov.v.simeon@gmail.com>
2022-12-11 23:45:29 +0200

    feat(11): Christmas tree lights

---
+++ 11-christmas-tree-lights/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <ChristmasTree :size="7">
+      <template #lights>
+        <ChristmasLights v-for="i in 2" :key="i" />
+      </template>
+    </ChristmasTree>
+  </div>
+</template>
+
+<script setup>
+import ChristmasTree from './ChristmasTree.vue'
+import ChristmasLights from './ChristmasLights.vue'
+</script>
---
+++ 11-christmas-tree-lights/src/ChristmasTree.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ChristmasTree v-if="size > 1" :size="size - 1">
+      <template #lights>
+        <slot name="lights" />
+      </template>
+    </ChristmasTree>
+
+    <div class="flex flex-row justify-center">
+      <div v-for="i in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center">
+        <slot name="lights" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  }
+)
+</script>
